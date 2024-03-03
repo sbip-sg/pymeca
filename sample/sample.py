2a81f1afae73633bc03160a309438a30f0e4aca4
hugy718 <hugy718@gmail.com>
2024-03-03 16:27:48 +0800

    Fix grammar

--- sample/sample.py
+++ sample/sample.py
@@ -19,8 +19,8 @@ from pymeca.utils import generate_meca_simulate_accounts
 accounts = generate_meca_simulate_accounts()
 print(accounts)
 
-# MECA DAO initiator deploys all contract and publish the addresses
-# in this sample we assume doa_owner is the initiator and has deployed the contract and published the below addresses
+# MECA DAO initiator deploys all contract and publishes the addresses
+# In this sample, we assume doa_owner is the initiator and has deployed the contract and published the below addresses
 
 addresses = {
     "dao_contract_address": "0x7829ad34690d2E8f5559F0499Fed0DE08D3A8137",
@@ -46,7 +46,7 @@ meca_dao_owner = MecaDAOOwner(
 )
 
 # %%
-# other MECA DAO entities joins the DAO with their on-chain identity
+# Other MECA DAO entities join the DAO with their on-chain identity
 
 from pymeca.user import MecaUser
 from pymeca.host import MecaHost
@@ -93,7 +93,7 @@ meca_tower.register_tower(
 )
 
 # %%
-# Task developer upload a task
+# Task developer uploads a task
 meca_task_developer.register_task(
     ipfs_sha256="0x" + "1" * 64,
     fee=10,
@@ -102,25 +102,25 @@ meca_task_developer.register_task(
 )
 
 # %%
-# host register its connection with tower
+# host registers its connection with tower
 print(meca_host.get_my_towers())
 meca_host.register_for_tower(tower_address=meca_tower.account.address)
 
 # %%
-# tower register a host to itself
+# tower registers a host to itself
 print(meca_tower.get_pending_hosts())
 meca_tower.accept_host(host_address=meca_host.account.address)
 print(meca_tower.get_my_hosts())
 
 # %%
-# host register a task
+# host registers a task
 print(meca_host.get_my_towers())
 print(meca_host.get_my_tasks())
 meca_host.add_task(ipfs_sha256="0x" + "1" * 64, block_timeout=7, fee=10)
 print(meca_host.get_my_tasks())
 
 # %%
-# meca user check the blockchain to determine the tower and host
+# meca user checks the blockchain to determine the tower and host
 towers_hosts = meca_user.get_towers_hosts_for_task(
     ipfs_sha256="0x" + "1" * 64,
 )
@@ -135,43 +135,43 @@ selected_tower, selected_host = select_tower_and_host(towers_hosts)
 print(selected_tower, selected_host)
 
 # %%
-# meca user propose a task request with the selected tower and host.
+# meca user proposes a task request with the selected tower and host.
 success, task_id = meca_user.send_task_on_blockchain(
     ipfs_sha256="0x" + "1" * 64,
     host_address=selected_host,
     tower_address=selected_tower,
     input_hash="0x" + "8" * 64,
 )
-# the input is encrypted with the hosts public key and send to the tower
+# the input is encrypted with the hosts public key and sent to the tower
 
 # %%
 # execution of the task
 # 1. tower receives the task request from user and stores the input locally (not shown here)
-# 2. tower check the task to ensure it is valid (Validity can include tower address, host address etc.)
+# 2. tower checks the task to ensure it is valid (Validity can include tower address, host address etc.)
 print(meca_tower.get_running_task(task_id=task_id))
-# 3. tower send the task to the host (not shown here)
-# 4. host retrieve the input from tower and check the task is valid (not shown here)
+# 3. tower sends the task to the host (not shown here)
+# 4. host retrieves the input from tower and checks the task is valid (not shown here)
 print(meca_host.get_running_task(task_id=task_id))
-# 5. host execute the task (not shown here)
-# 6. host store the output locally (not shown here)
+# 5. host decrypts the input with its private key and executes the task (not shown here)
+# 6. host stores the output locally (not shown here)
 
 
 # %%
-# meca user pull the results from the tower
-# meca user acknowledge the task request completion
+# meca user pulls the results from the tower
+# meca user acknowledges the task request completion
 meca_user.finish_task(task_id=task_id)
 
 # %%
 # participants can leave the MECA DAO
 
 # %%
-# host delete its active task, empty its queue and leave MECA DAO
+# host deletes its active task, empties its queue and leaves MECA DAO
 meca_host.delete_task(ipfs_sha256="0x" + "1" * 64)
 print(meca_host.get_my_tasks())
 meca_host.update_block_timeout_limit(new_block_timeout_limit=0)
 meca_host.unregister()
 
 # %%
-# tower delete empty its queue and leave MECA DAO
+# tower empties its queue and leave MECA DAO
 meca_tower.update_tower_size_limit(new_size_limit=0)
 meca_tower.delete_tower()
